5c8646450e3dc2aac983c2bf0ef6b705c286ce0d
David Morales <morgar.creative@gmail.com>
2017-10-25 23:48:32 -0500

    Need to work on link to gifs

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,5 @@
 // Create an array of strings, each one related to a topic that interests you. Save it into a variable called "topics"
 
-
 // Your app should take the topics in this array and create buttons in your HTML.
 
 // When tyhe user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on a page
@@ -12,35 +11,104 @@
 // Add a form to your page takes the value from the user input box and adds it into your "topics" array. Then make a -function call- that takes each topic in the array remakes the buttons on the page.
 
 $(document).ready(function() {
-    
+
 	var topics = ["dog", "lion", "eagle", "bear"];
 
-		
-	function renderButtons(){
-	
-		$("#topics-view").empty();
+	function gifs(){
+		$("#topicView").empty();
 
-	for (var i = 0; i < topics.lenght; i++){
-		
-		var mkButton = $("<button>");
-			mkButton.addClass("topic");
-			mkButton.attr("data-name", topics[i]);
-			mkButton.append(topics[i]);
-		
-		$("#topics-view").append(a);
+		var animalsClicked = $(this).attr("data-name");
+
+		var jqxhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + animalsClicked + "&api_key=9dDor3gmivO76TVIyLdGL84slUIBchH5&limit=10");
+
+		jqxhr.done(function(data) { 
+
+			for (var i = 0; i < 10; i++){
+
+				console.log(data);
+
+				var createDiv = $("<div>");
+				createDiv.addClass("gifWrap");
+
+				var addRating = $("<h4>");
+				addRating.append("rating: " + data.data[i].rating);
+
+				//Need to 
+				var gif = $("<img>");
+				gif.attr("data-gif1", data.data[i].images.fixed_height_still.url)
+				gif.attr("data-gif2", data.data[i].images.fixed_height.url)
+				gif.attr("scr", data.data[i].images.fixed_height_still.url)
+
+				createDiv.append(gif)
+				createDiv.append(addRating)
+
+				$("#topicView").append(createDiv)
+
+				makeButtons();
+			}
+
+			$("img").click(function(){
+
+				if($(this).attr("scr") === $(this).attr("data-gif1")){
+
+					$(this).attr("scr", $(this).attr("data-gif2"))
+				}
+				else{
+					$(this).attr("scr", $(this).attr("data-gif1"))
+				}
+			})
+
+
+			console.log("success got data", data);
+
+		});
 
 
 	}
 
-		
-}
+	function makeButtons(){
 
-	$("#find-topic").on("click", function(event){
-		event.preventDefault();
-		var topic = $("#topic-input").val().trim();
-		topics.push(topic);
-		renderButtons();
+		$("#topicButtons").empty();
+
+		for (var i = 0; i < topics.length; i++){
+
+			var getButton = $("<button>");
+
+			getButton.addClass("animalButton")
+
+			getButton.attr("data-name", topics[i]);
+
+			getButton.append(topics[i]);
+
+			$("#topicButtons").append(getButton);
+		}
+	}
+
+	$("#searchAnimal").on("click", function(event){
+
+		if ($("#inputLabel").val() === ""){
+			alert("Plase input an animal")
+		}
+
+		else{
+
+			topics.push($("#inputLabel").val())
+
+			console.log(topics)
+
+			event.preventDefault();
+
+			$("#topicButtons").empty();
+			$("#inputLabel").empty()
+
+			makeButtons();
+
+		}
 	});
 
-	renderButtons();
+	$(document).on("click", ".animalButton", gifs);
+
+	makeButtons();
+
 });
+
